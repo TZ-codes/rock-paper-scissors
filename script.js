a3680ad8ae77bcd4622af0347ba9bcd71b981a07
Tyler <Faulknert2016@gmail.com>
2022-08-17 14:20:31 -0400

    created function playRound that successfully tests all possible outcomes of a game of rock, paper, scissors

--- script.js
+++ script.js
@@ -14,5 +14,32 @@ const getComputerChoice = () => {
     }
 };
 
+//create a function, playRound, that takes the parameters playerSelection and computerSelection
+//Function needs to return a string that declares the winner of a round (i.e. "You Lose! Paper beats Rock")
+//For the playerSelection parameter, you need it to be case-insensitive (i.e. rock, Rock, RocK)
+//Should consider all possible outcomes like a tie, playerSelection is Rock, Paper, or Scissors
+const playRound = (playerSelection, computerSelection) => {
+    if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {
+        return "Tie!";
+    } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection === "Scissors") {
+        return "You Win! Rock beats Scissors."
+    } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection === "Rock") {
+        return "You Win! Paper beats Rock."
+    } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection === "Paper") {
+        return "You win! Scissors beats Paper."
+    } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection === "Paper") {
+        return "You Lose! Paper beats Rock."
+    }  else if (playerSelection.toUpperCase() === "PAPER" && computerSelection === "Scissors") {
+        return "You Lose! Scissors beats Paper."
+    }  else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection === "Rock") {
+        return "You Lose! Rock beats Scissors."
+    } else {
+        return "Error! Please try again."
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
 
 
