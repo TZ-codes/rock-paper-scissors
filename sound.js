a87d1e7df5c8688206c4524654695d45fedf7260
Tyler <Faulknert2016@gmail.com>
2022-09-05 20:31:37 -0400

    made the buttons bigger and centered the text/buttons

--- sound.js
+++ sound.js
@@ -1,15 +1,33 @@
-let muteBtn = document.querySelector('.mute-btn');
-const blueBird = document.querySelector('sound');
+const muteBtn = document.querySelector(".mute-btn"); 
+const myAudio = document.getElementById("myAudio");
+
+isPlaying = false;
+
+const togglePlay = () => {
+  if (myAudio.paused) {
+      myAudio.play(); 
+  } else {
+      myAudio.pause(); 
+  }
+}
+
 
 muteBtn.addEventListener('click', function() {
   const icon = this.querySelector('i');
 
-  if (icon.classList.contains('fa-volume-high')) {
-    icon.classList.remove('fa-volume-high');
-    icon.classList.add('fa-volume-xmark');
-  } else {
+  if (icon.classList.contains('fa-volume-xmark')) {
     icon.classList.remove('fa-volume-xmark');
     icon.classList.add('fa-volume-high');
+  } else {
+    icon.classList.remove('fa-volume-high');
+    icon.classList.add('fa-volume-xmark');
   }
 
+  togglePlay();
+
 });
+
+myAudio.addEventListener("ended", () => {
+  myAudio.currentTime = 0; 
+  myAudio.play();
+})
